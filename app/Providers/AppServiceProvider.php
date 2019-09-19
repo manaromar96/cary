67f7ea558e8944597ea2e943f2940b8ab71056c4
Islam Mezied <imezied@gmail.com>
2019-09-19 13:02:28 +0300

    fix and resolve store observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Store::observe(StoreObserver::class);
     }
 }
